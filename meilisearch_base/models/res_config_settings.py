8568fcd22b2a3828921536469319a85932a8a945
Janik von Rotz <login@janikvonrotz.ch>
2024-08-28 17:06:08 +0200

    fix: module name for settings page

--- meilisearch_base/models/res_config_settings.py
+++ meilisearch_base/models/res_config_settings.py
@@ -1,11 +1,10 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    module_meilisearch = fields.Boolean("Integrate with Meilisearch")
+    module_meilisearch_base = fields.Boolean("Integrate with Meilisearch")
     meilisearch_api_url = fields.Char(
         "Meilisearch API Url", config_parameter="meilisearch.api_url"
     )
