ed0f92de906d5a4fcc5d477a0b1b0d38e5245621
ulrich.kilian@juleg.ch <ulrich.kilian@juleg.ch>
2024-10-30 08:08:05 +0100

    feat: kardex stock move section improved

--- stock_kardex/models/stock_picking.py
+++ stock_kardex/models/stock_picking.py
@@ -1,19 +1,92 @@
 import logging
 
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
 
+PICKING_DATE_HANDLING = 'send' # or 'create'
+
 
 class StockPicking(models.Model):
-    _inherit = ['stock.picking']
+    _name = 'stock.picking'
+    _inherit = ['stock.picking', 'base.kardex.mixin']
     _description = 'Stock Kardex Picking'
 
     kardex = fields.Boolean(string='Kardex', default=False)
+    kardex_id = fields.Integer(string='Kardex Id')
+    kardex_done = fields.Boolean(string="in Kardex bekannt", default=False)
+    kardex_row_create_time = fields.Char(string="Kardex Row_Create_Time")
+    kardex_row_update_time = fields.Char(string="Kardex Row_Update_Time")
+    kardex_status = fields.Selection(selection=[('0', 'Ready'), ('1', 'Pending'), ('2', 'Success'), ('3', 'Error')], default='0', string="Kardex STATUS")
 
     def send_to_kardex(self):
-        pass
+        
+        for picking in self:
+            picking_vals = picking.read()[0]
+            # get moves belonging to this picking
+            moves = self.env['stock.move'].search([('picking_id', '=', picking.id)])
+            if not moves:
+                raise ValidationError('No moves found for this picking')
+            check_moves_counter = 0
+            check_moves_list = []
+            for move in moves:
+                print(f"Product: {move.product_id.name}")
+                if not move.product_id.kardex_done:
+                    check_moves_counter += 1
+                    check_moves_list.append(move.product_id.name)
+
+            if check_moves_counter > 0:
+                raise ValidationError(f"These Products are unknown in Kardex: {', '.join(check_moves_list)}. Please transfer Products to Kardex.")
+                return
+
+            # create external object for every picking record 
+            for move in moves:
+                table = 'PPG_Auftraege'
+                # add ID of products zo picking vals
+                picking_vals['kardex_product_id'] = move.product_id.kardex_id
+                #create_time, update_time = self._get_dates(move, PICKING_DATE_HANDLING)
+                #picking_vals['kardex_row_create_time'] = create_time
+                #picking_vals['kardex_row_update_time'] = update_time
+                picking_vals['kardex_status'] = '1'
+                picking_vals['kardex_unit'] = move.product_id.uom_id.name
+                picking_vals['kardex_quantity'] = move.quantity
+                new_id, create_time, update_time = self._create_external_object(picking_vals, table)
+                # new_id = self._create_external_object(picking_vals, table)
+                
+                _logger.info('new_id: %s' % (new_id,))
+
+                
+                done_move = {
+                    'kardex_done': True,
+                    'kardex_id': new_id,
+                    'kardex_status': '1',
+                    'kardex_row_create_time': create_time,
+                    'kardex_row_update_time': update_time
+                    }
+                move.write(done_move)
+                message = 'Kardex Picking was sent to Kardex.'
+
+            done_picking = {
+                    'kardex_done': True,
+                    'kardex_row_create_time': create_time,
+                    'kardex_row_update_time': update_time
+                }
+            picking.write(done_picking)    
+            return self._create_notification(message)
+
+    @api.model
+    def write(self, vals):
+        res = super(StockPicking, self).write(vals)
+        
+        # Check if the 'kardex_done' field is being updated
+        if 'kardex_done' in vals:
+            for picking in self:
+                # Update the 'kardex' field in related stock.move records
+                picking.move_ids.write({'kardex_done': vals['kardex_done']})
+
+        return res
+            
 
     
 
@@ -25,6 +98,12 @@ class StockMove(models.Model):
         string='Product',
         domain="[('kardex', '=', parent.kardex)]",
     ) # this adds domain to existing domain!
+    kardex_id = fields.Integer(string='Kardex Id')
+    kardex_done = fields.Boolean(string="in Kardex bekannt", default=False)
+    kardex_row_create_time = fields.Char(string="Kardex Row_Create_Time")
+    kardex_row_update_time = fields.Char(string="Kardex Row_Update_Time")
+    kardex_status = fields.Selection(selection=[('0', 'Ready'), ('1', 'Pending'), ('2', 'Success'), ('3', 'Error')], default='0', string="Kardex STATUS")
+
 
 
     @api.depends('picking_id.kardex')
@@ -32,7 +111,6 @@ class StockMove(models.Model):
         # if picking is kardex than product must be kardex too
         # could also be done by onchange but dynamic domain setting by onchange method seems to deprecated in odoo 17
         # field products_domain must be included in view
-        _logger.info('compute method called')
         for obj in self:
             if obj.picking_id.kardex:
                 domain = [('kardex', '=', 'True')]
