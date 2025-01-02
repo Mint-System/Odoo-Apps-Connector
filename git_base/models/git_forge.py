33b9bd86fb263c035bf3c8386dccde7f48801571
Janik von Rotz <login@janikvonrotz.ch>
2025-01-02 10:52:37 +0100

    feat: import repos from local path

--- git_base/models/git_forge.py
+++ git_base/models/git_forge.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from odoo import fields, models
 from odoo.tools import config
@@ -24,3 +25,53 @@ class GitForge(models.Model):
     def _compute_local_path(self):
         for rec in self:
             rec.local_path = f"{config['data_dir']}/git/{rec.hostname}"
+
+    def _import_repos_from_local_path(self):
+        imported_repos = []
+        for forge_id in self:
+
+            # First level in local path are accounts
+            accounts = [
+                f
+                for f in os.listdir(forge_id.local_path)
+                if os.path.isdir(os.path.join(forge_id.local_path, f))
+            ]
+            for account in accounts:
+                local_path = os.path.join(forge_id.local_path, account)
+                account_id = self.env["git.account"].search(
+                    [("name", "=", account)], limit=1
+                )
+                if not account_id:
+                    account_id = self.env["git.account"].create(
+                        {"name": account, "forge_id": forge_id.id}
+                    )
+
+                # Second level in local path are repos
+                repos = [
+                    f
+                    for f in os.listdir(local_path)
+                    if os.path.isdir(os.path.join(local_path, f))
+                ]
+                for repo in repos:
+                    repo_id = self.env["git.repo"].search(
+                        [("name", "=", repo)], limit=1
+                    )
+                    if not repo_id:
+                        repo_id = self.env["git.repo"].create(
+                            {"name": repo, "account_id": account_id.id}
+                        )
+                        imported_repos.append(repo_id.name)
+
+        return imported_repos
+
+    def action_import_repos_from_local_path(self):
+        import_repos = self._import_repos_from_local_path()
+        return {
+            "type": "ir.actions.client",
+            "tag": "display_notification",
+            "params": {
+                "title": "Repositories Imported",
+                "message": f"Successfully imported {len(import_repos)} repositorie(s).",
+                "type": "success",
+            },
+        }
