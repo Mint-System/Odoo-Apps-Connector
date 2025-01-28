6d830ec92e14db170fa4d21412bca928c0f9f8ba
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:40:05 +0100

    chore: update author

--- base_kardex/__manifest__.py
+++ base_kardex/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Provides Basic Kardex Functionality.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Services,Helpdesk,Manufacturing,Website,Inventory,Administration,Productivity",
     "version": "18.0.1.0.0",
--- git_base/__manifest__.py
+++ git_base/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Manage git repositories with Odoo.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Technical",
     "version": "18.0.1.0.0",
