bdb628ea590e9c25c74ff5af3dd981bac3c27e39
Ulrich Kilian <uk@science-and-more.de>
2025-03-11 09:58:49 +0100

    feat: create stock lot model

--- stock_kardex/models/stock_picking.py
+++ stock_kardex/models/stock_picking.py
@@ -736,3 +736,13 @@ class StockQuant(models.Model):
 
         
         return True
+
+class StockLot(models.Model):
+    _name = "stock.lot" 
+    _description = "Stock Update Lot"
+    _inherit = ["stock.lot"]
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        return super().create(vals_list)
