b5d687a8ef7527e255f56fb6ec2b3b9f7349c0be
Ulrich Kilian <uk@science-and-more.de>
2025-10-27 20:14:14 +0100

    fix(product_kardex): manifest dependencies

--- product_kardex/__manifest__.py
+++ product_kardex/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Stock",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["product"],
+    "depends": ["product", "base_kardex"],
     "data": [
         "views/product_kardex_views.xml",
     ],
